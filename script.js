a184d2dd876ca7f71f49aa0d13a30e76e5468718
ChristineWest <christinewwwest@gmail.com>
2022-09-07 16:22:45 -0400

    fixed hamburger menu, media queries, sticky nav

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Hamburger menu rom codepen.io
 //https://codepen.io/sitanotern1337/pen/xyQppZ?editors=0110
-const hamburger = document.querySelector('.hamburger');
-const hiddenNav = document.querySelector('.navBar');
-const burgerBox = document.querySelector('.burgerContainer');
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('isactive');
-    hiddenNav.classList.toggle('active');
+const hamburger = document.querySelector(".hamburger");
+const slideOutUl = document.querySelector(".mobileNavBar ul");
+const burgerBox = document.querySelector(".burgerContainer");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("isactive");
+  burgerBox.classList.toggle("active");
+});
+slideOutUl.addEventListener("click", function () {
+  hamburger.classList.toggle("isactive");
+  burgerBox.classList.toggle("active");
 });
 
 // Project slides carousel
@@ -15,66 +19,74 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName('mySlides');
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++){
-        slides[i].style.display = 'none';
-    }
-    slides[slideIndex - 1].style.display = 'block';
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
-
 
 // Typerwriter effect learned from youtube
 // https://www.youtube.com/watch?v=9T4YTUeDgwU&t=8s
 function typeWriterEffect() {
-    const words = ['creative', 'passionate', 'curious', 'tenacious', 'articulate'];
+  const words = [
+    "creative",
+    "passionate",
+    "curious",
+    "tenacious",
+    "articulate",
+  ];
 
-    let wordCount = 0;
-    let letterCount = 0;
+  let wordCount = 0;
+  let letterCount = 0;
 
-    let currentText = '';
-    let currentWord = '';
+  let currentText = "";
+  let currentWord = "";
 
-    let timeOut = 400;
-    
-    let isDeleting = false;
+  let timeOut = 400;
 
-    function type() {
-        if (wordCount === words.length) {
-            wordCount = 0;
-        }
-        currentWord = words[wordCount];
+  let isDeleting = false;
 
-        if (isDeleting) {
-            currentText = currentWord.slice(0, --letterCount);
-        } else {
-            currentText = currentWord.slice(0, ++letterCount);
-        }
-
-        document.querySelector('.typewrite').textContent = currentText;
-
-        timeOut = isDeleting ? 200 : 400;
-
-        if (!isDeleting && currentText.length === currentWord.length) {
-            timeOut = 2000;
-            isDeleting = true;
-        } else if (isDeleting && currentText.length === 0) {
-            timeOut = 1000;
-            isDeleting = false;
-            wordCount++;
-        }
-        setTimeout(type, timeOut);
+  function type() {
+    if (wordCount === words.length) {
+      wordCount = 0;
     }
-    type();
+    currentWord = words[wordCount];
+
+    if (isDeleting) {
+      currentText = currentWord.slice(0, --letterCount);
+    } else {
+      currentText = currentWord.slice(0, ++letterCount);
+    }
+
+    document.querySelector(".typewrite").textContent = currentText;
+
+    timeOut = isDeleting ? 200 : 400;
+
+    if (!isDeleting && currentText.length === currentWord.length) {
+      timeOut = 2000;
+      isDeleting = true;
+    } else if (isDeleting && currentText.length === 0) {
+      timeOut = 1000;
+      isDeleting = false;
+      wordCount++;
+    }
+    setTimeout(type, timeOut);
+  }
+  type();
 }
 typeWriterEffect();
-
